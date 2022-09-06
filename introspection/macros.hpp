9436679c3c04a8e498adf79585bcbc1c4acee0b0
Samuel B. Johnson <sabjohnso@yahoo.com>
2022-09-05 20:18:00 -0500

    :sparkles: Introspection data mamcro
    Problem:
    - introspection didn't have a light weight macro to add introspection
    data.
    
    Solution:
    - Add one

--- introspection/macros.hpp
+++ introspection/macros.hpp
@@ -1,7 +1,35 @@
 #pragma once
+//
+// ... standard header files
+//
+#include <boost/preprocessor.hpp>
+#include <string_view>
 
-#define INSPECTABLE(...)                                               \
-   struct Metadata {                                                   \
-      static constexpr ::std::tuple member_pointers{};                 \
-      static constexpr ::std::tuple member_names{};                    \
+#define __S_QUOTE_AUX(...) #__VA_ARGS__
+#define __S_QUOTE(a, b, ...)                                                   \
+   std::string_view { __S_QUOTE_AUX(__VA_ARGS__) }
+
+#define __S_POP_AUX(...) __VA_ARGS__
+#define __S_POP(...) __S_POP_AUX __VA_ARGS__
+
+#define __S_SEQ_TO_VARIADIC(seq) __S_POP(BOOST_PP_SEQ_TO_TUPLE(seq))
+
+#define __S_TRANSFORM(OP, data, ...)                                           \
+   __S_SEQ_TO_VARIADIC(                                                        \
+     BOOST_PP_SEQ_TRANSFORM(OP, data, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)))
+
+#define __S_MEMBER_POINTER(_ignored, struct_name, member_name)                 \
+   &struct_name::member_name
+
+#define __S_MEMBER_POINTERS(struct_name, ...)                                  \
+   __S_TRANSFORM(__S_MEMBER_POINTER, struct_name, __VA_ARGS__)
+
+#define __S_MEMBER_NAMES(...) __S_TRANSFORM(__S_QUOTE, , __VA_ARGS__)
+
+#define INTROSPECTION_DATA(name, ...)                                          \
+   struct Metadata {                                                           \
+      static constexpr ::std::tuple member_pointers{                           \
+        __S_MEMBER_POINTERS(name, __VA_ARGS__)};                               \
+      static constexpr ::std::tuple member_names{                              \
+        __S_MEMBER_NAMES(__VA_ARGS__)};                                        \
    }
--- introspection_testing/macros_test.cpp
+++ introspection_testing/macros_test.cpp
@@ -9,22 +9,33 @@
 #include <introspection/introspection.hpp>
 #include <introspection/macros.hpp>
 
+#define QUOTE_AUX(...) #__VA_ARGS__
+#define QUOTE(...) QUOTE_AUX(__VA_ARGS__)
+
 namespace introspection::testing {
 
    struct Thing {
-      INSPECTABLE(
+      std::array<int, 2> member1{};
+      int member2{};
+      double member3{};
 
-        ((std::array<int, 2>), member1, {}),
-
-        (int, member2, {}),
-
-        (double, member3, [ 2 ] {}));
+      INTROSPECTION_DATA(Thing, member1, member2, member3);
    };
 
-   TEST(macro, introspective) {
-      ASSERT_TRUE(concepts::Introspective<Thing>);
+   TEST(macro, introspective) { ASSERT_TRUE(concepts::Introspective<Thing>); }
+
+   TEST(macro, member_count) { ASSERT_EQ(member_count<Thing>, 3); };
+
+   TEST(macro, member_names) {
+      ASSERT_EQ((member_name<Thing, 0>), std::string_view("member1"));
+      ASSERT_EQ((member_name<Thing, 1>), std::string_view("member2"));
+      ASSERT_EQ((member_name<Thing, 2>), std::string_view("member3"));
    }
 
-   TEST(macro, member_count) { ASSERT_EQ(member_count<Thing>, 3); }
+   TEST(macro, member_types) {
+      ASSERT_TRUE((std::same_as<Member_type<Thing, 0>, std::array<int, 2>>));
+      ASSERT_TRUE((std::same_as<Member_type<Thing, 1>, int>));
+      ASSERT_TRUE((std::same_as<Member_type<Thing, 2>, double>));
+   }
 
 } // end of namespace introspection::testing
